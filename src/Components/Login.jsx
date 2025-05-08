b188e741029a9212f0419cdccd301657c5180c42
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-08 15:49:25 +0500

    Added Input Field For User Name

---
+++ src/Components/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../Context/UserContext";
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+
+    
